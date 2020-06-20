43ec014584b12727b3816b766686c95e97930cc2
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-20 06:47:16 +0100

    Add loading spinner and views for other states

--- app/src/main/java/com/electroninc/newsfeeds/activities/NewsActivity.java
+++ app/src/main/java/com/electroninc/newsfeeds/activities/NewsActivity.java
@@ -8,8 +8,9 @@ import androidx.loader.content.Loader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.electroninc.newsfeeds.R;
 import com.electroninc.newsfeeds.network.NewsLoader;
@@ -20,13 +21,18 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     private static final int LOADER_ID = 1;
 
     private TextView searchResultTextView;
-    // https://content.guardianapis.com/search?q=term&api-key=test
+    private TextView noResultTextView;
+    private TextView notConnectedTextView;
+    private ProgressBar loadingProgressSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
         searchResultTextView = findViewById(R.id.search_result_text_view);
+        noResultTextView = findViewById(R.id.no_result_text_view);
+        notConnectedTextView = findViewById(R.id.not_connected_text_view);
+        loadingProgressSpinner = findViewById(R.id.loading_progress_spinner);
 
         Intent displayNewsIntent = getIntent();
         LoaderManager.getInstance(this).initLoader(LOADER_ID, displayNewsIntent.getExtras(), this);
@@ -46,9 +52,34 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     public void onLoadFinished(@NonNull Loader<String> loader, String data) {
         searchResultTextView.setText(data);
+        showSearchResults();
     }
 
     @Override
     public void onLoaderReset(@NonNull Loader<String> loader) {
+    }
+
+    private void showSearchResults() {
+        searchResultTextView.setVisibility(View.VISIBLE);
+        hideViews(noResultTextView, notConnectedTextView, loadingProgressSpinner);
+    }
+
+    private void showNoResult() {
+        noResultTextView.setVisibility(View.VISIBLE);
+        hideViews(searchResultTextView, notConnectedTextView, loadingProgressSpinner);
+    }
+
+    private void showNotConnected() {
+        notConnectedTextView.setVisibility(View.VISIBLE);
+        hideViews(searchResultTextView, noResultTextView, loadingProgressSpinner);
+    }
+
+    private void showLoadingProgress() {
+        loadingProgressSpinner.setVisibility(View.VISIBLE);
+        hideViews(searchResultTextView, noResultTextView, notConnectedTextView);
+    }
+
+    private void hideViews(View... views) {
+        for (View view : views) view.setVisibility(View.GONE);
     }
 }
