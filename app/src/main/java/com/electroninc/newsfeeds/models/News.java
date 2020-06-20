70b0092c7732feedf3ddf9549ae4308a94b826b8
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-20 13:41:23 +0100

    Created POJO for News

--- app/src/main/java/com/electroninc/newsfeeds/models/News.java
+++ app/src/main/java/com/electroninc/newsfeeds/models/News.java
@@ -1,4 +1,31 @@
 package com.electroninc.newsfeeds.models;
 
 public class News {
+    private String title;
+    private String url;
+    private String section;
+    private String date;
+
+    public News(String title, String url, String section, String date) {
+        this.title = title;
+        this.url = url;
+        this.section = section;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
